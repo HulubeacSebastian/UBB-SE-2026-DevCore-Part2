b5a9a91c415917c42d2b8f061997ef3ebd6c8108
raluca.pascuta7 <raluca.pascuta@stud.ubbcluj.ro>
2026-03-30 00:15:52 +0300

    Add database connection class

---
+++ src/KarmaBanking.App/Data/DatabaseConnection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace KarmaBanking.App.Data
+{
+    public class DatabaseConnection
+    {
+        private readonly string _connectionString;
+
+        public DatabaseConnection()
+        {
+            _connectionString = "Server=localhost;Database=KarmaBanking;Trusted_Connection=True;TrustServerCertificate=True;";
+        }
+
+        public SqlConnection GetDatabaseConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
