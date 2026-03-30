19c212e31cc4972a89fa4745d5e21699c295b945
raluca.pascuta7 <raluca.pascuta@stud.ubbcluj.ro>
2026-03-30 18:17:14 +0300

    BA-4: Re-add GetSavingsAccountsByUserIdAsync lost during merge

--- src/KarmaBanking.App/Repositories/Interfaces/ISavingsRepository.cs
+++ src/KarmaBanking.App/Repositories/Interfaces/ISavingsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using KarmaBanking.App.Models;
 
 namespace KarmaBanking.App.Repositories.Interfaces
@@ -6,8 +7,8 @@ namespace KarmaBanking.App.Repositories.Interfaces
     public interface ISavingsRepository
     {
         Task<bool> AddSavingsAccountAsync(SavingsAccount savingsAccount);
+        Task<List<SavingsAccount>> GetSavingsAccountsByUserIdAsync(int userId);
         Task<bool> UpdateSavingsAccountBalanceAsync(int savingsAccountId, decimal amountToAdd);
-
         Task<bool> CloseSavingsAccountAsync(int savingsAccountId);
     }
 }
