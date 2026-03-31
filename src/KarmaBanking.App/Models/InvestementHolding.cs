955fe3784f367466974a6d94cb7024ed3fc998c9
MatiaOpris <rev.oprismatia@gmail.com>
2026-04-01 00:31:02 +0300

    Created Invetesment Holding

---
+++ src/KarmaBanking.App/Models/InvestementHolding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarmaBanking.App.Models
+{
+    internal class InvestementHolding
+    {
+        public int Id { get; set; }
+        public int PortfolioId { get; set; }
+        public string Ticker { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Quantity { get; set; }
+        public decimal AvgPurchasePrice { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+
+        public decimal UnrealizedGainLoss { get; set; }
+    }
+}
